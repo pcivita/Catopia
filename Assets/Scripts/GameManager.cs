efb62419098bb205b71a3c8ac154d21a471d8fc0
melissaran <39605081+melissaran@users.noreply.github.com>
2024-11-13 15:16:40 -0800

    Create GameState class, modify GameManager
    GameState handles the game state independent of visualization. Game Manager interfaces with Gameobjects and ScriptableObjects.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,23 +6,46 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] CatSO defaultCat1Data;
-    [SerializeField] CatSO defaultCat2Data;
-    // If there are problems: Change to GameObject
-    [SerializeField] Cat catPrefab;
+    public static GameManager instance;
+    GameState gameState;
+    [SerializeField] CatSO[] defaultCats;
+    [SerializeField] GameObject catPrefab;
 
+    List<Cat> catInstances;
 
     private void Start()
     {
-        CreateDefaultCats();
+        instance = this;
+        catInstances = new List<Cat>();
+
+        //create new game state
+        gameState = GameState.NewGame();
+
+        //construct all cats
+        foreach(var c in gameState.GetCats()){
+            ConstructCat(c);
+        }
     }
 
+    public CatSO[] GetDefaultCats()
+    {
+        return defaultCats;
+    }
+
+    void ConstructCat(CatSO c)
+    {
+        Cat newCat = Instantiate(catPrefab, Vector2.left, quaternion.identity).GetComponent<Cat>();
+        newCat.Init(c);
+        catInstances.Add(newCat);
+    }
+    
+
+    /*
     void CreateDefaultCats()
     {
-        
-        Instantiate(catPrefab, Vector2.left, quaternion.identity).Init(defaultCat1Data);
-        Instantiate(catPrefab, Vector2.right, quaternion.identity).Init(defaultCat2Data);
-        Instantiate(catPrefab, Vector2.left * 2, quaternion.identity).Init(defaultCat1Data);
-        Instantiate(catPrefab, Vector2.right * 2, quaternion.identity).Init(defaultCat2Data);
-    }
+        Instantiate(catPrefab, Vector2.left, quaternion.identity).Init(defaultCatData[0]);
+        Instantiate(catPrefab, Vector2.right, quaternion.identity).Init(defaultCatData[1]);
+        Instantiate(catPrefab, Vector2.left * 2, quaternion.identity).Init(defaultCatData[0]);
+        Instantiate(catPrefab, Vector2.right * 2, quaternion.identity).Init(defaultCatData[1]);
+    }*/
 }
---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//All data that needs to be stored about our game. Do not use for visualization purposes.
+public class GameState{
+
+    protected List<CatSO> colonyMembers;
+    protected int foodCount;
+
+    //NOT FINAL!
+    //TODO how do we want to seed the colony? could randomize
+    static List<CatSO> InitalizeCats()
+    {
+        Debug.Log("Initialize cats");
+        List<CatSO> l = new List<CatSO>();
+        l.Add(GameManager.instance.GetDefaultCats()[0]);
+        l.Add(GameManager.instance.GetDefaultCats()[1]);
+        return l;
+    }
+
+    public static GameState NewGame()
+    {
+        Debug.Log("Call new game");
+        GameState state = new GameState();
+        state.colonyMembers = InitalizeCats();
+        state.foodCount = 5;
+        return state;
+    }
+
+    public List<CatSO> GetCats()
+    {
+        return colonyMembers;
+    }
+
+    public void KillCat(CatSO c)
+    {
+        Debug.Log("Killed cat " + c.CatName);
+        colonyMembers.Remove(c);
+    }
+
+    public void AddCat(CatSO c)
+    {
+        Debug.Log("Add cat " + c.CatName);
+        colonyMembers.Remove(c);
+    }
+
+    public int GetFood()
+    {
+        return foodCount;
+    }
+
+    public bool TryConsumeFood(int amount)
+    {
+        if (amount > foodCount)
+        {
+            return false;
+        }
+        else
+        {
+            foodCount -= amount;
+            return true;
+        }
+    }
+
+    public void AddFood(int amount)
+    {
+        foodCount += amount;
+    }
+
+
+}
