967fde9b00fce707601b28fd3b4d02c711738d9a
pcivita <pcivita@stanford.edu>
2024-11-13 19:41:58 -0800

    TextMeshPro And Cat UI

--- Assets/Scripts/Areas/HuntArea.cs
+++ Assets/Scripts/Areas/HuntArea.cs
@@ -1,11 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class HuntArea : AreaController
 {
     public int totalHunting;
+    private TMP_Text catTextMesh;
+    private TMP_Text huntTextMesh;
 
+    private void Start()
+    {
+        TMP_Text[] textMeshes = gameObject.GetComponentsInChildren<TMP_Text>();
+        Debug.Log(textMeshes.Length);
+        
+        if (textMeshes.Length >= 2)
+        {
+            catTextMesh = textMeshes[0];
+            huntTextMesh = textMeshes[1];
+        }
+        catTextMesh.text = "Cat: " + _cats.Count;
+        huntTextMesh.text = "Hunt: " + totalHunting;
+    }
 
     public override void UpdateAreaState(Cat cat, bool addingCat)
     {
@@ -17,6 +34,8 @@ public class HuntArea : AreaController
         {
             totalHunting -= cat._catSO.Hunting;
         }
+        catTextMesh.text = "Cat: " + _cats.Count;
+        huntTextMesh.text = "Hunt: " + totalHunting;
     }
 
     public override void NewDay()
@@ -24,5 +43,7 @@ public class HuntArea : AreaController
         GameManager.instance.gameState.AddFood(totalHunting);
         totalHunting = 0;
         _cats.Clear();
+        catTextMesh.text = "Cat: " + _cats.Count;
+        huntTextMesh.text = "Hunt: " + totalHunting;
     }
 }
